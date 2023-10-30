295740b2418f9d4d0b8d58d9ea3095e03e63a0ff
Cee2021 <ua_05@ymail.com>
2023-10-30 15:57:07 -0700

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,16 +6,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
+void close_elf(int elf)
+{
+    close(elf);
+}
+
+void print_magic()
+{
+    /* Function implementation ... */
+}
+
+void print_class()
+{
+    /* Function implementation ... */
+}
+
+void print_data()
+{
+    /* Function implementation ... */
+}
+
+void print_version()
+{
+    /* Function implementation ... */
+}
+
+void print_abi()
+{
+    /* Function implementation ... */
+}
+
+void print_osabi()
+{
+    /* Function implementation ... */
+}
+
+void print_type(unsigned int e_type)
+{
+    /* Function implementation ... */
+    printf("Type: %u\n", e_type);
+}
+
+void print_entry(unsigned long int e_entry)
+{
+    /* Function implementation ... */
+    printf("Entry: %lu\n", e_entry);
+}
 
 /* Declare the dprintf function */
 int dprintf(int fd, const char *format, ...);
@@ -75,14 +111,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
     check_elf(header->e_ident);
     printf("ELF Header:\n");
-    print_magic(header->e_ident);
-    print_class(header->e_ident);
-    print_data(header->e_ident);
-    print_version(header->e_ident);
-    print_osabi(header->e_ident);
-    print_abi(header->e_ident);
-    print_type(header->e_type, header->e_ident);
-    print_entry(header->e_entry, header->e_ident);
+    print_magic();
+    print_class();
+    print_data();
+    print_version();
+    print_osabi();
+    print_abi();
+    print_type(header->e_type);
+    print_entry(header->e_entry);
 
     free(header);
     close_elf(o);
