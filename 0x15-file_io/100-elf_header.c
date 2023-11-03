2c64b8a748daaf69ccfcd2ded5a66f8ae7237f3b
Cee2021 <ua_05@ymail.com>
2023-11-03 15:21:03 -0700

    header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,78 +1,61 @@
-/*
-** 100-elf_header.c
-**
-** Display the information contained in the ELF header at the start of an ELF file.
-**
-** Usage: elf_header elf_filename
-**
-** Format: the same as readelf -h (version 2.26.1)
-**
-** If the file is not an ELF file, or on error, exit with status code 98 and display a comprehensive error message to stderr.
-**
-** You are allowed to use lseek once and read a maximum of two times at runtime.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
 
-static void print_elf_header(const Elf64_Ehdr *ehdr)
-{
-	printf("Magic:   %08x\n", ehdr->e_ident[EI_MAG0] |
-	                       (ehdr->e_ident[EI_MAG1] << 8) |
-	                       (ehdr->e_ident[EI_MAG2] << 16) |
-	                       (ehdr->e_ident[EI_MAG3] << 24));
-	printf("Class:    %d\n", ehdr->e_ident[EI_CLASS]);
-	printf("Data:     %d\n", ehdr->e_ident[EI_DATA]);
-	printf("Version:  %d\n", ehdr->e_ident[EI_VERSION]);
-	printf("OS/ABI:    %d\n", ehdr->e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", ehdr->e_ident[EI_ABIVERSION]);
-	printf("Type:      %d\n", ehdr->e_type);
-	printf("Entry point address: %016lx\n", ehdr->e_entry);
+void display_error(const char *message) {
+    fprintf(stderr, "Error: %s\n", message);
+    exit(98);
 }
 
-int main(int argc, char *argv[])
-{
-	FILE *fp;
-	Elf64_Ehdr ehdr;
+void display_elf_header(const char *filename) {
+    Elf32_Ehdr elf_header;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: elf_header elf_filename\n");
-		return EXIT_FAILURE;
-	}
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        display_error("Failed to open file");
+    }
 
-	fp = fopen(argv[1], "rb");
-	if (fp == NULL)
-	{
-		fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
-		return EXIT_FAILURE;
-	}
+    if (read(fd, &elf_header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
+        close(fd);
+        display_error("Failed to read ELF header");
+    }
 
-	/* Seek to the ELF header. */
-	lseek(fileno(fp), 0, SEEK_SET);
+    if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
+        elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+        elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+        elf_header.e_ident[EI_MAG3] != ELFMAG3) {
+        close(fd);
+        display_error("Not an ELF file");
+    }
 
-	/* Read the ELF header. */
-	if (fread(&ehdr, sizeof(ehdr), 1, fp) != 1)
-	{
-		fprintf(stderr, "Error reading ELF header: %s\n", strerror(errno));
-		return EXIT_FAILURE;
-	}
+    /* Display ELF header information */
+    printf("Magic: %02x %02x %02x %02x\n",
+           elf_header.e_ident[EI_MAG0],
+           elf_header.e_ident[EI_MAG1],
+           elf_header.e_ident[EI_MAG2],
+           elf_header.e_ident[EI_MAG3]);
 
-	fclose(fp);
+    printf("Class: %d\n", elf_header.e_ident[EI_CLASS]);
+    printf("Data: %d\n", elf_header.e_ident[EI_DATA]);
+    printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
+    printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
+    printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
+    printf("Type: %d\n", elf_header.e_type);
+    printf("Entry point address: %lx\n", (unsigned long)elf_header.e_entry);
 
-	/* Check if the file is an ELF file. */
-	if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0)
-	{
-		fprintf(stderr, "File '%s' is not an ELF file\n", argv[1]);
-		return EXIT_FAILURE;
-	}
-
-	print_elf_header(&ehdr);
-
-	return EXIT_SUCCESS;
+    close(fd);
 }
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        display_error("Usage: elf_header elf_filename");
+    }
+
+    display_elf_header(argv[1]);
+
+    return 0;
+}
+
 
